062f82091866ae226a0cab7c06a5503de77ba649
Alexander Touhdown <echan@necrofant.asia>
2016-04-21 00:56:35 +0200

    Home Controller up and running!

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,8 +1,14 @@
 <?php
 
-namespace app\Http\Controllers;
+namespace Instyle\Http\Controllers;
 
-abstract class Controller extends BaseController
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Auth\Access\AuthorizesResources;
+
+class Controller extends BaseController
 {
-    use DispatchesJobs, ValidatesRequests;
+    use AuthorizesRequests, AuthorizesResources, DispatchesJobs, ValidatesRequests;
 }
--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace app\Http\Controllers;
-class HomeController extends BaseController
+namespace Instyle\Http\Controllers;
+
+class HomeController extends Controller
 {
     public function index()
     {
