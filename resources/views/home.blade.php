2034b5122c4281a128d47789e478b9766088caf5
Alexander Touhdown <echan@necrofant.asia>
2016-04-26 10:36:14 +0200

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,12 @@
 @extends('templates.default')
 
 @section('content')
-    <h1>instyle.social</h1>
+<div class="well">
+    <well>
+        <h1>instyle.social</h1>
     <h3>La red social 'a la última'.</h3>
+    </well>
+</div>
+
 
 @stop
