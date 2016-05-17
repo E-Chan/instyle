c1c877b7df0291b64de50d676f00b7d4a4395aed
Alexander Touhdown <echan@necrofant.asia>
2016-05-17 15:01:08 +0200

    Creado formulario de posts y migración posts

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,7 +2,36 @@
 
 @section('content')
     @if (Auth::check()) 
-        <h1>Timeline<h1>
+        <div class="row" id="timeline">
+            <div class="col-md-3" id="padding">
+            </div>
+
+            <div class="col-md-6">
+                <div class="row" id="newPost">
+                    <h3> New post </h3>
+                    <form style="margin: 0; padding=0;" action="#">
+                        <div class="form-group" style="white-space: normal">
+                            <textarea class="form-control"  cols="35" rows="2" maxlength="600" placeholder="¿Que estilo luces hoy?" required></textarea>
+                        </div>
+                    </form>
+                        <button style="display: inline;" type="button" class="btn-lg btn-default">
+                        <span class="glyphicon glyphicon-plus"></span>
+                        <span class="glyphicon glyphicon-camera"></span>
+                        </button>
+                        <input style="display: inline;" type="submit" value="Subir" class="btn btn-primary btn-block">
+                </div>
+
+                <div class="row" id="posts">
+                    <h1>Timeline</h1>
+
+                </div>
+            </div>
+
+            <div class="col-md-3" id="padding">
+            </div>
+
+
+        </div>
     @else
         <h1>Bienvenido a instyle.social</h1>
         <h3>La red social 'a la última'.</h3>
--- resources/views/timeline.blade.php
+++ resources/views/timeline.blade.php
@@ -1,7 +1,23 @@
 @extends('templates.default')
 
 @section('content')
-    <h1>Timeline</h1>
-    <h3></h3>
+    <div class="row" id="timeline">
+        <div class="col-md-3" id="padding">
+        </div>
+        <div class="col-md-6">
+            <div class="row" id="newPost">
+                <h1> new post </h1>
+            </div>
+
+            <div class="row" id="posts">
+                <h1>Timeline</h1>
+
+            </div>
+        </div>
+        <div class="col-md-3" id="padding">
+        </div>
+
+
+    </div>
 
 @stop
