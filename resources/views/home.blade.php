9a2f038cbd568bf14d8cd2fdcf7d90818b557e31
echan42 <echan@necrofant.asia>
2016-09-05 03:29:56 +0200

    :)

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -28,7 +28,7 @@
                     </div>
                     <div class="media-body">
                         <div class="user">
-                             <a href="http://instyle.social/user/@{{ post.user.username }}"><strong>@{{ post.user.username }}</strong></a> - @{{ post.relativeCreatedAt }}
+                             <a href="user/@{{ post.user.username }}"><strong>@{{ post.user.username }}</strong></a> - @{{ post.relativeCreatedAt }}
                     
                         </div>
                         <p>@{{ post.body }}</p>
